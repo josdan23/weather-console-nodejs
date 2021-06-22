1b5759cf87d25512d5abf65a1a8e639885800e97
Daniel Yapura <danielyapura@gmail.com>
2021-06-22 15:02:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### About
+nodejs console application to find geographic and weather information about a city.
+
+
+
+### How to install?:
+
+1. Donwload and unzip.
+
+2. Install dependecies:
+
+   `npm install`
+
+3. Run app: 
+
+   `node app.js`
---
+++ models/busquedas.js
@@ -0,0 +1,130 @@
+const fs = require('fs');
+
+const axios = require('axios').default;
+
+class Busquedas {
+
+    historial = [];
+    dbPath = './db/database.json';
+
+    constructor() {
+        this.leerDB();
+    }
+
+    get historialCapitalizado() {
+
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1) );
+
+            return palabras.join(' ');
+        })
+    }
+
+    get paramsMapbox() {
+        return {
+            'access_token': process.env.MAPBOX_KEY,
+            'language': 'es',
+            'limit': 5
+        };
+    }
+
+    get paramsOpenWeather() {
+        return {
+            'appid': process.env.OPENWEATHER_KEY,
+            'units': 'metric',
+            'lang': 'es',
+        }
+    }
+
+    async ciudad( lugar = '') {
+
+        //peticion http
+        const instance = axios.create({
+            baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+            params: this.paramsMapbox
+        });
+
+        const resp = await instance.get();
+
+        return resp.data.features.map( lugar => ({
+
+            id: lugar.id,
+            nombre: lugar.place_name,
+            lng: lugar.center[0],
+            lat: lugar.center[1],
+
+        }));
+
+
+        return []; // retornar los lugares 
+
+    }
+
+    async climaLugar( lat, lon ){
+
+        try {
+            
+            // instance axios.create
+            const instance = axios.create({
+                baseURL: `http://api.openweathermap.org/data/2.5/weather`,
+                params: { ...this.paramsOpenWeather, lat, lon }
+            });
+    
+            // resp.data
+            const resp = await instance.get();
+
+            return {
+                desc: resp.data.weather[0].description,
+                min: resp.data.main.temp_min,
+                max: resp.data.main.temp_max,
+                temp: resp.data.main.temp 
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    agregarHistorial( lugar = '' ) {
+
+        if ( this.historial.includes( lugar.toLowerCase() ) ) {
+            return ;
+        }
+
+        this.historial = this.historial.splice(0,5);
+        
+        this.historial.unshift( lugar.toLowerCase() );
+
+        // guarda en la db
+        this.guardarDB( lugar );
+    }
+
+    guardarDB() {
+
+        const payload = {
+            historial: this.historial
+        };
+        
+        fs.writeFileSync( this.dbPath, JSON.stringify( payload ));
+
+    }
+
+    leerDB() {
+        
+        if ( !fs.existsSync( this.dbPath ))
+            return; 
+
+        const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8'});
+
+        const data = JSON.parse( info );
+
+        this.historial = data.historial;
+
+    }
+
+}
+
+
+module.exports = Busquedas;
